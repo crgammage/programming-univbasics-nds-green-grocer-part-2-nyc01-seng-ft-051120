0bc051fedf8bc0cd1f1c9eb9e9eab8270031adc5
Christa Gammage <crgammage@gmail.com>
2020-05-06 21:19:02 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -46,7 +46,7 @@ def checkout(cart, coupons)
   total = 0
   index = 0
   while index < final_cart.length
-    total += final_cart[index][:price] * final_cart[index]
+    total += final_cart[index][:price] * final_cart[index][:count]
     index += 1
   end
 end
