98e8efdf66fe1b9ce69d2c9d02c00c90fa7cdbbe
Christa Gammage <crgammage@gmail.com>
2020-05-06 21:16:44 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -41,4 +41,6 @@ end
 def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart)
+  clearance_cart = apply_clearance(couponed_cart)
+  
 end
